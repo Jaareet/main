4eed848d4ff1cbc31049b002baa067dc01992b37
Jaareet <80985233+Jaareet@users.noreply.github.com>
2022-01-04 19:00:33 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -425,3 +425,93 @@ function svEastwood(){
         `
     )
 }
+
+function bandolerosView(){
+    $("body").html(
+        `
+        <div class="info">
+            <div class="container">
+                <div class="title mobile">
+                    Jaareet's Web - BandolerosRP
+                    <hr>
+                    <div class="subtitle" style="position:absolute;left:50%;top:80%;transform:translateX(-50%);font-size: .89vw;">
+                        <span class="sub">
+                            Estos son los servicios a los que puedes conectarte
+                        </span>
+                    </div>
+                </div>
+                <div class="buttons" align = "center">
+                    <button><a href = "https://discord.gg/FTpYPvJmjJ">Discord</a></button>
+                    <br>
+                    <button><a onClick="Web()">Website</a></button>
+                    <br>
+                    <button><a onClick="svBandoleros()">Servidor</a></button>
+                    <br>
+                    <button><a href = "index.html">Volver</a></button>
+                </div>
+            </div>
+        </div>
+        <style>
+            @media only screen and (max-width: 375px) {
+                .container .mobile {
+                    width: max-content;
+                    font-size: 4.5vw;
+                    padding: 10vw 0;
+                }
+                .subtitle {
+                    width: 70vw;
+                    margin: -5vw 0;
+                }
+                .sub {
+                    font-size: 3.5vw;
+                }
+            }
+        </style>
+        `
+    )
+}
+
+function svBandoleros(){
+    $("body").html(
+        `
+        <div class="info">
+            <div class="container">
+                <div class="title mobile">
+                    Jaareet's Web - BandolerosRP
+                    <hr>
+                    <div class="subtitle" style="position:absolute;left:50%;top:80%;transform:translateX(-50%);font-size: .89vw;">
+                        <span class="sub">
+                            Estos son los servicios a los que puedes conectarte
+                        </span>
+                    </div>
+                </div>
+                <div class="buttons" align = "center">
+                    <button><a href="fivem://connect/cfx.re/join/5ajlg7">Conectarse</a></button>
+                    <br>
+                    <button><a href="https://servers.fivem.net/servers/detail/5ajlg7">Información</a></button>
+                    <br>
+                    <button><a href = "index.html">Volver</a></button>
+                </div>
+            </div>
+        </div>
+
+        <style>
+            @media only screen and (max-width: 375px) {
+                .container .mobile {
+                    width: max-content;
+                    font-size: 4.5vw;
+                    padding: 10vw 0;
+                }
+                .subtitle {
+                    width: 70vw;
+                    margin: -5vw 0;
+                }
+                .sub {
+                    font-size: 3.5vw;
+                }
+            }
+        </style>
+
+        `
+    )
+}
